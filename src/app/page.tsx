e03ea57fcbf37a50f867ac547ccb6cd750f19f26
wangkx <1989163733@qq.com>
2024-07-16 16:38:50 +0800

    fix:change index height

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import NavBar from "@/app/components/layout/NavBar";
 
 export default function Home() {
   return (
-    <div className="h-screen w-full">
+    <div className="w-full" style={{height:`calc(100vh - 64px)`}}>
       <NavBar />
       <div className="w-full 2xl:max-w-7xl h-full 2xl:mx-auto py-4 box-border">
         Index
