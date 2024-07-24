2f928b3bb2710f2800cd1320e35c8f96fe05ff88
wangkexing <1989163733@qq.com>
2024-07-24 22:40:15 +0800

    styles:edit login page

--- src/app/login/components/LoginForm.tsx
+++ src/app/login/components/LoginForm.tsx
@@ -1,29 +1,42 @@
-'use client';
-import React from 'react';
+"use client";
+import React from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
 import { Button } from "@radix-ui/themes";
-
+import { IconUser, IconKey } from "@douyinfe/semi-icons";
 
 interface IFormInput {
-    firstName: string
-    password: string
+  username: string;
+  password: string;
 }
 
-
 export default function LoginForm() {
+  const { register, handleSubmit } = useForm<IFormInput>();
+  const onSubmit: SubmitHandler<IFormInput> = (data) => console.log(data);
 
-    const { register, handleSubmit } = useForm<IFormInput>()
-    const onSubmit: SubmitHandler<IFormInput> = (data) => console.log(data)
-
-    return (
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <label>UserName</label>
-            <input {...register("username", { required: true, maxLength: 20 })} />
-            <label>PassWord</label>
-            <input {...register("password", { required: true, minLength: 6, maxLength: 18 })} />
-            <Button color="cyan" variant="soft" size={"2"} >
-                <input type="submit" value="登录"/>
-            </Button>
-        </form>
-    )
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className="flex items-center">
+        <label className="mr-2">
+          <IconUser size="large"/>
+        </label>
+        <input {...register("username", { required: true, maxLength: 20 })} className="bg-gray-200 outline-none px-2 py-1"/>
+      </div>
+      <div className="mt-4 flex items-center">
+        <label className="mr-2">
+          <IconKey size="large"/>
+        </label>
+        <input
+          {...register("password", {
+            required: true,
+            minLength: 6,
+            maxLength: 18,
+          })}
+          className="bg-gray-200 outline-none px-2 py-1"
+        />
+      </div>
+      <Button color="cyan" variant="soft" size={"2"}>
+        <input type="submit" value="登录" />
+      </Button>
+    </form>
+  );
 }
