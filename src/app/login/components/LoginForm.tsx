5b3f1bc3a01c4205dc0a64883f60e6d971e049ab
wangkx <1989163733@qq.com>
2024-07-25 17:57:19 +0800

    styles:edit login page

--- src/app/login/components/LoginForm.tsx
+++ src/app/login/components/LoginForm.tsx
@@ -1,42 +1,44 @@
 "use client";
 import React from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
-import { Button } from "@radix-ui/themes";
+import { Button } from "@douyinfe/semi-ui";
 import { IconUser, IconKey } from "@douyinfe/semi-icons";
 
 interface IFormInput {
-  username: string;
-  password: string;
+    username: string;
+    password: string;
 }
 
 export default function LoginForm() {
-  const { register, handleSubmit } = useForm<IFormInput>();
-  const onSubmit: SubmitHandler<IFormInput> = (data) => console.log(data);
+    const { register, handleSubmit } = useForm<IFormInput>();
+    const onSubmit: SubmitHandler<IFormInput> = (data) => console.log(data);
 
-  return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <div className="flex items-center">
-        <label className="mr-2">
-          <IconUser size="large"/>
-        </label>
-        <input {...register("username", { required: true, maxLength: 20 })} className="bg-gray-200 outline-none px-2 py-1"/>
-      </div>
-      <div className="mt-4 flex items-center">
-        <label className="mr-2">
-          <IconKey size="large"/>
-        </label>
-        <input
-          {...register("password", {
-            required: true,
-            minLength: 6,
-            maxLength: 18,
-          })}
-          className="bg-gray-200 outline-none px-2 py-1"
-        />
-      </div>
-      <Button color="cyan" variant="soft" size={"2"}>
-        <input type="submit" value="登录" />
-      </Button>
-    </form>
-  );
+    return (
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="flex items-center">
+                <label className="mr-3 text-white">
+                    <IconUser size="large" />
+                </label>
+                <input {...register("username", { required: true, maxLength: 20 })} className="bg-white outline-none px-3 py-1" />
+            </div>
+            <div className="mt-4 flex items-center">
+                <label className="mr-3 text-white">
+                    <IconKey size="large" />
+                </label>
+                <input
+                    {...register("password", {
+                        required: true,
+                        minLength: 6,
+                        maxLength: 18,
+                    })}
+                    className="bg-white outline-none px-3 py-1"
+                />
+            </div>
+            <div className="mt-4 text-center">
+                <Button theme='solid' type='primary'>
+                    登录
+                </Button>
+            </div>
+        </form>
+    );
 }
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,17 +1,14 @@
 import React from 'react'
 import LoginForm from './components/LoginForm';
-import navbarStyles from '@/app/components/Layout/navbar.module.css';
 // import Image from 'next/image'
 // import loginBg from '@/app/images/login.jpg'
-
-const titleStyles = `text-4xl font-bold text-center mb-6 mt-6 italic ${navbarStyles["gradient-text-login"]}`
 
 export default function Logn() {
   return (
     <div className='flex justify-center items-center h-screen bg-gradient-to-r from-cyan-100 to-blue-100'>
       {/* <Image src={loginBg} alt="login" objectFit='cover' className='absolute w-full h-full' /> */}
-      <div className='w-1/4 min-h-64 bg-white rounded-md p-4 box-border shadow-xl flex flex-col items-center'>
-        <h1 className={titleStyles}>LOGIN</h1>
+      <div className='w-[30rem] min-h-64 bg-black/10 rounded-md p-4 box-border shadow-lg flex flex-col items-center'>
+        <h1 className="text-3xl font-bold text-center mb-6 mt-6 italic text-white">LOGIN</h1>
         <LoginForm />
       </div>
     </div>
