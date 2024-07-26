a675e64ec3442711c236ba86e693576ad8c4a7ba
wangkx <1989163733@qq.com>
2024-07-26 17:35:37 +0800

    chore:add form vailate rules

--- src/app/login/components/LoginForm.tsx
+++ src/app/login/components/LoginForm.tsx
@@ -10,46 +10,64 @@ interface IFormInput {
 }
 
 export default function LoginForm() {
+
     const { handleSubmit, formState: { errors }, control } = useForm<IFormInput>({
         defaultValues: {
             username: "",
             password: "",
         },
-        mode: "onBlur",
-
+        mode: "onBlur"
     });
-    const onSubmit: SubmitHandler<IFormInput> = (data) => console.log(errors);
+    const onSubmit: SubmitHandler<IFormInput> = (data) => console.log(data);
 
     return (
         <form onSubmit={handleSubmit(onSubmit)} method="POST">
             <div className="lg:flex bg-[#eff0f3] px-5 py-3 box-border rounded-md">
-                <Controller
-                    name="username"
-                    control={control}
-                    rules={{
-                        required: true,
-                        minLength: 3,
-                        maxLength: 18,
-                    }}
+                <div className="mt-4 lg:ml-4 lg:mt-0 w-full text-white">
+                    <Controller
+                        name="username"
+                        control={control}
+                        rules={{
+                            required: true,
+                            minLength: {
+                                value: 3,
+                                message: "用户名长度不能小于3位"
+                            },
+                            maxLength: {
+                                value: 18,
+                                message: "用户名长度不能大于18位"
+                            }
+                        }}
 
-                    render={({ field }) => <Input prefix={<IconUser />} placeholder={"请输入用户名"} style={{ backgroundColor: 'white' }} size="large" {...field} aria-invalid={errors.username ? "true" : "false"} />}
-                />
+                        render={({ field }) => (<Input prefix={<IconUser />} placeholder={"请输入用户名"} validateStatus={errors.username ? "error" : "success"}
+                            style={{ backgroundColor: 'white' }} size="large" {...field} aria-invalid={errors.username ? "true" : "false"} />)}
+                    />
+                    {errors.username && <span role="alert" className="text-red-500 text-sm">{errors.username.message}</span>}
+                </div>
                 <div className="mt-4 lg:ml-4 lg:mt-0 w-full text-white">
                     <Controller
                         name="password"
                         control={control}
                         rules={{
                             required: true,
-                            minLength: 6,
-                            maxLength: 18,
+                            minLength: {
+                                value: 6,
+                                message: "密码长度不能小于6位"
+                            },
+                            maxLength: {
+                                value: 18,
+                                message: "密码长度不能大于18位"
+                            }
                         }}
-                        render={({ field }) => <Input prefix={<IconKey />} placeholder={"请输入密码"} style={{ backgroundColor: 'white' }} size="large" {...field} mode="password" aria-invalid={errors.password ? "true" : "false"} />}
+                        render={({ field }) => (<Input prefix={<IconKey />} placeholder={"请输入密码"} validateStatus={errors.password ? "error" : "success"}
+                            style={{ backgroundColor: 'white' }} size="large" {...field} mode="password" aria-invalid={errors.password ? "true" : "false"} />)}
                     />
+                    {errors.password && <span role="alert" className="text-red-500 text-sm">{errors.password.message}</span>}
                 </div>
 
                 <div className="text-center mt-4 lg:ml-4 lg:mt-0">
-                    <Button theme='solid' size="large" type='primary' className="w-full">
-                        <input type="submit" value={'登录'} />
+                    <Button theme='solid' size="large" type='primary' htmlType="submit">
+                        登录
                     </Button>
                 </div>
             </div>
--- src/app/register/components/RegisterForm.tsx
+++ src/app/register/components/RegisterForm.tsx
@@ -2,7 +2,7 @@
 import React from "react";
 import { useForm, SubmitHandler, Controller } from "react-hook-form";
 import { Button, Input } from "@douyinfe/semi-ui";
-import { IconUser, IconKey,IconMail } from "@douyinfe/semi-icons";
+import { IconUser, IconKey, IconMail } from "@douyinfe/semi-icons";
 
 interface IFormInput {
     username: string;
@@ -22,33 +22,48 @@ export default function LoginForm() {
         mode: "onBlur",
 
     });
-    const onSubmit: SubmitHandler<IFormInput> = (data) => console.log(errors);
+    const onSubmit: SubmitHandler<IFormInput> = (data) => console.log(data);
 
     return (
         <form onSubmit={handleSubmit(onSubmit)} method="POST">
             <div className="bg-[#eff0f3] px-5 py-3 box-border rounded-md">
-                <Controller
-                    name="username"
-                    control={control}
-                    rules={{
-                        required: true,
-                        minLength: 3,
-                        maxLength: 18,
-                    }}
-
-                    render={({ field }) => <Input prefix={<IconUser />} placeholder={"请输入用户名"} style={{ backgroundColor: 'white' }} size="large" {...field} aria-invalid={errors.username ? "true" : "false"} />}
-                />
+                <div className="mt-4 w-full text-white">
+                    <Controller
+                        name="username"
+                        control={control}
+                        rules={{
+                            required: true,
+                            minLength: {
+                                value: 3,
+                                message: "用户名长度不能小于3位"
+                            },
+                            maxLength: {
+                                value: 18,
+                                message: "用户名长度不能大于18位"
+                            }
+                        }}
+                        render={({ field }) => <Input prefix={<IconUser />} placeholder={"请输入用户名"} style={{ backgroundColor: 'white' }} size="large" {...field} aria-invalid={errors.username ? "true" : "false"} />}
+                    />
+                    {errors.username && <span role="alert" className="text-red-500 text-sm">{errors.username.message}</span>}
+                </div>
                 <div className="mt-4 w-full text-white">
                     <Controller
                         name="password"
                         control={control}
                         rules={{
                             required: true,
-                            minLength: 6,
-                            maxLength: 18,
+                            minLength: {
+                                value: 6,
+                                message: "密码长度不能小于6位"
+                            },
+                            maxLength: {
+                                value: 18,
+                                message: "密码长度不能大于18位"
+                            }
                         }}
                         render={({ field }) => <Input prefix={<IconKey />} placeholder={"请输入密码"} style={{ backgroundColor: 'white' }} size="large" {...field} mode="password" aria-invalid={errors.password ? "true" : "false"} />}
                     />
+                    {errors.password && <span role="alert" className="text-red-500 text-sm">{errors.password.message}</span>}
                 </div>
 
                 <div className="mt-4 w-full text-white">
@@ -57,11 +72,18 @@ export default function LoginForm() {
                         control={control}
                         rules={{
                             required: true,
-                            minLength: 6,
-                            maxLength: 18,
+                            minLength: {
+                                value: 6,
+                                message: "密码长度不能小于6位"
+                            },
+                            maxLength: {
+                                value: 18,
+                                message: "密码长度不能大于18位"
+                            }
                         }}
                         render={({ field }) => <Input prefix={<IconKey />} placeholder={"请再次输入密码"} style={{ backgroundColor: 'white' }} size="large" {...field} mode="password" aria-invalid={errors.passwordConfirm ? "true" : "false"} />}
                     />
+                    {errors.passwordConfirm && <span role="alert" className="text-red-500 text-sm">{errors.passwordConfirm.message}</span>}
                 </div>
 
                 <div className="mt-4 w-full text-white">
@@ -69,17 +91,16 @@ export default function LoginForm() {
                         name="mail"
                         control={control}
                         rules={{
-                            required: true,
-                            minLength: 6,
-                            maxLength: 18,
+                            required: true
                         }}
                         render={({ field }) => <Input prefix={<IconMail />} placeholder={"请输入邮箱"} style={{ backgroundColor: 'white' }} size="large" {...field} aria-invalid={errors.mail ? "true" : "false"} />}
                     />
+                    {errors.mail && <span role="alert" className="text-red-500 text-sm">{errors.mail.message}</span>}
                 </div>
 
                 <div className="text-center mt-4 ">
-                    <Button theme='solid' size="large" type='primary' className="w-full">
-                        <input type="submit" value={'注册'} />
+                    <Button theme='solid' size="large" type='primary' className="w-full" htmlType="submit">
+                        注册
                     </Button>
                 </div>
             </div>
